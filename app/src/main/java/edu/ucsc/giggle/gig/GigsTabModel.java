bf478c7a8cf52a13e3785d799d23a503f2e7e2d6
JordanBJones <jbjones@ucsc.edu>
2016-11-16 16:35:19 -0800

    Jordan's branch
    working on user input and putting that in the database

--- app/src/main/java/edu/ucsc/giggle/gig/GigsTabModel.java
+++ app/src/main/java/edu/ucsc/giggle/gig/GigsTabModel.java
@@ -1,14 +1,12 @@
 package edu.ucsc.giggle.gig;
 
-
-
 /**
  * Created by JanJan on 10/10/2016. OG tab code by Jan
  */
 public class GigsTabModel {
     public static final String[] data = {
-            "Bananarchy + Saturday's Sunset \n\t\tWhere: Johnny V's \n\t\tWhen: 11/18/16 \n\t\tTime: 8pm"
+            "Bananarchy + Saturday's Sunset \n\t\tWhere: Johnny V's \n\t\tWhen: 11/18/16 \n\t\tTime: 8pm",
+            "Bananarchy + Adult School \n\t\tWhere: Rosie McCann's \n\t\tWhen: 11/19/16 \n\t\tTime: 8pm"
     };
-
 }
 
